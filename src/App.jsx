8e42ac1b77a64ec0a403289e19c4b18830b11cb6
Student <student@Administrators-Mac-mini.local>
2025-03-21 14:28:10 +0200

    ülessanne 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,28 @@ import './App.css'
 import ExpenseItem from './components/ExpenseItem'
 function App() {
   const [count, setCount] = useState(0)
-
+  const expenses = [
+    {
+      date: new Date(2025, 3, 21),
+      title: 'New book',
+      price: 30.99
+    },
+    {
+      date: new Date(2028, 3, 21),
+      title: 'Old book',
+      price: 10.99
+    }
+    ]
+  
   return (
     <>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        data={expenses[0]}
+      />
+      <ExpenseItem
+        data={expenses[1]}
+      />
+        
     </>
   )
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import "./ExpenseItem.css"
+const ExpenseItem = (props) => {
+    
 
-const ExpenseItem = () => {
   return (
-    <h2>ExpenseItem</h2>
+    <div className='expense-item'>
+        <div className='expense-item_date'>{props.data.date.toString()}</div>
+        <div className='expense-item_description'>
+            <h2>{props.data.title}</h2>
+            <div className='expense-item_price'>{props.data.price}</div>
+        </div>
+    </div>
   )
 }
 
