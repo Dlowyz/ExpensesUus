6c8b107d8fed49a827f00305eb4df39b56d8ab08
Student <student@Administrators-Mac-mini.local>
2025-03-21 14:47:42 +0200

    ülessanne 4

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./ExpenseItem.css"
+import ExpenseDate from './ExpenseDate'
 const ExpenseItem = (props) => {
     
-
   return (
     <div className='expense-item'>
-        <div className='expense-item_date'>{props.data.date.toString()}</div>
+        <div className='expense-item_date'>
+            <ExpenseDate date={props.data.date}/>
+        </div>
         <div className='expense-item_description'>
             <h2>{props.data.title}</h2>
             <div className='expense-item_price'>{props.data.price}</div>
