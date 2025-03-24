cb845620a9570b9d9aad3e5940857f4788048e79
Dlowyz <166107299+Dlowyz@users.noreply.github.com>
2025-03-24 17:08:43 +0200

    ülessanne 12

---
+++ src/components/Expenses/ExpenseFilter.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ExpenseFilter.css';
+
+const ExpensesFilter = (props) => {
+
+  const dropdownChangeHandler = (event) => {
+    const selectedYear = event.target.value;
+    props.filter(selectedYear);
+  };
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={dropdownChangeHandler}>
+          <option value='2023'>2023</option>
+          <option value='2024'>2024</option>
+          <option value='2025'>2025</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ExpenseItem from './ExpenseItem'
 import "./Expenses.css"
 import ExpenseDate from './ExpenseDate'
+import ExpenseFilter from './ExpenseFilter'
 import Card from '../UI/Card'
 const Expenses = (props) => {
+  const ExpenseFilterHandler = (data) => {
+  console.log('Year data in Expenses.jsx '+data) 
+  }
   return (
     <Card className='expenses'>
+      <ExpenseFilter filter={ExpenseFilterHandler}/>
       <ExpenseItem data = {props.data[0]}/>
       <ExpenseItem data = {props.data[1]}/>
     </Card>
