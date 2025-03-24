3efa720304f0810907e1ff07b5149a85569be0de
Dlowyz <166107299+Dlowyz@users.noreply.github.com>
2025-03-24 16:32:36 +0200

    ülessanne 11.2

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,24 +1,75 @@
+import { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = (props) => {
+    const [userInput, setUserInput] = useState(
+        {
+            enteredTitle: '',
+            enteredPrice: '',
+            enteredDate: ''
+        }
+    )
+
+    const TitleChangeEvent = (event) => {
+        setUserInput({
+            ...userInput, 
+            enteredTitle: event.target.value
+        })
+        console.log(userInput)
+        console.log(userInput[0])
+    }
+    const PriceChangeEvent = (event) => {
+        setUserInput({
+            ...userInput, 
+            enteredPrice: event.target.value
+        })
+        console.log(userInput)
+    }
+    const DateChangeEvent = (event) => {
+        setUserInput({
+            ...userInput, 
+            enteredDate: event.target.value
+            
+        })
+        console.log(userInput)
+    }
+    const SubmitClickEvent = (event) => {
+        event.preventDefault()
+        console.log(userInput)
+        const ExpenseData = {
+            title: userInput.enteredTitle,
+            price: userInput.enteredPrice,
+            date: new Date(userInput.enteredDate)
+        }
+        
+        props.onSaveExpenseData(ExpenseData)
+
+        setUserInput({
+            enteredTitle: '',
+            enteredPrice: '',
+            enteredDate: ''
+        })
+        
+    }
+
   return (
     <form>
   <div className="new-expense-controls">
     <div className="new-expense-control">
       <label>Title </label>
-      <input type="text" />
+      <input type="text"  onChange={TitleChangeEvent} value={userInput.enteredTitle}/>
     </div>
     <div className="new-expense-control">
       <label>Price </label>
-      <input type="number" min="0.01" step="0.01" />
+      <input type="number" min="0.01" step="0.01" onChange={PriceChangeEvent} value={userInput.enteredPrice} />
     </div>
     <div className="new-expense-control">
       <label>Date </label>
-      <input type="date" min="2024-11-12" max="2026-01-31" />
+      <input type="date" min="2024-11-12" max="2026-01-31" onChange={DateChangeEvent} value={userInput.enteredDate ? new Date(userInput.enteredDate).toISOString().split('T')[0] : ''}/>
     </div>
   </div>
   <div className="new-expense-actions">
-    <button type="submit">Add Expense</button>
+    <button type="submit" onClick={SubmitClickEvent}>Add Expense</button>
   </div>
     </form>
 
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -3,9 +3,16 @@ import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 import Card from '../UI/Card'
 const NewExpense = (props) => {
+    const saveExpenseDataHandler = (savedExpenseData) => {
+        const expenseData = {
+            ...savedExpenseData,
+            id: Math.random().toString()
+        }
+        console.log(expenseData)
+    }
   return (
     <Card className='NewExpenseDiv'>
-      <ExpenseForm></ExpenseForm>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
     </Card>
   )
 }
